00d1ed12202b23c43ff1398a345f1c2aa09f9122
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2014-03-19 11:14:48 +0900

    Add function `instant-markdown:refresh-if-buffer-modified`

--- instant-markdown.el
+++ instant-markdown.el
@@ -61,6 +61,16 @@
         (callback (or cb #'instant-markdown:default-callback)))
     (url-retrieve (instant-markdown:request-url) callback)))
 
+(defvar instant-markdown:buffer-modified-tick-last nil)
+
+(defun instant-markdown:refresh-if-buffer-modified ()
+  (interactive)
+  (let ((modified-tick (buffer-modified-tick)))
+    (when (or (not (numberp instant-markdown:buffer-modified-tick-last))
+              (/= instant-markdown:buffer-modified-tick-last modified-tick))
+      (setq instant-markdown:buffer-modified-tick-last modified-tick)
+      (instant-markdown:refresh))))
+
 ;;;###autoload
 (defun instant-markdown:refresh ()
   (interactive)
